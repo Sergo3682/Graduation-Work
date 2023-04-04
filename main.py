7a88f2a1d963773125d4bdd78d02e1b0f2bd8122
Sergey Rogov <rogovsergey2001@gmail.com>
2023-04-04 14:22:32 +0300

    creating parser instance

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from image_net import NetImage
 from parser import Parser
 
 if __name__ == '__main__':
+
     bv = BitVector(0b00101101, 8)
     tt = TruthTable(bv, ['A', 'B', 'C'])
     sb = SchematicBuilder(tt)
     sb.build_pull_down_network()
     sb.build_pull_up_network()
+    p = Parser('netlist.sp', sb, '~/VSU224_Sky130_fd_pr/models/sky130.lib.spice')
