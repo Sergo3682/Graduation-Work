979e1a0b2394db75291fe1c2708cf2dd76b1f547
Sergey Rogov <rogovsergey2001@gmail.com>
2023-04-10 14:44:49 +0300

    added method for parsing single instance on branch

--- parser.py
+++ parser.py
@@ -1,4 +1,7 @@
 from schematicbuilder import SchematicBuilder
+from serialnodes import SerialNodes
+from net import Net
+from elems import Instance
 from net_walker import NetWalker
 
 
@@ -13,22 +16,57 @@ class Parser:
             self.pu_root = builder.pu_netlist[0]
             self.builder = builder
         if lib is not None:
-            self.lib = '.lib ' + lib
-            self.nfet = 'sky130_fd_pr__nfet_g5v0d10v5 w=1 l=1'
-            self.pfet = 'sky130_fd_pr__pfet_g5v0d10v5 w=1 l=1'
+            self.lib = '.lib ' + lib + ' tt'
+            self.nfet = 'sky130_fd_pr__nfet_g5v0d10v5 w=20 l=3'
+            self.pfet = 'sky130_fd_pr__pfet_g5v0d10v5 w=60 l=3'
+        self.pd_instances = []
+        self.name_idx = 0
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.output_file_name}, {self.builder.__repr__()})'
 
-    def not_creating(self):
-        res = []
-        nw = NetWalker(self.pd_root)
-        print(f'{type(self.pd_root)}\t\t{self.pd_root}')
-        nw.walk(self.pd_root, sn_action_fn=None, sn_fn_kwargs=None, net_action_fn=self.builder.get_inverted_inputs,
-                net_fn_kwargs={'res_lst': res})
-        nw.walk(self.pu_root, sn_action_fn=None, sn_fn_kwargs=None, net_action_fn=self.builder.get_inverted_inputs,
-                net_fn_kwargs={'res_lst': res})
-        return res
+    # def not_creating(self):
+    #     res = []
+    #     nw = NetWalker(self.pd_root)
+    #     print(f'{type(self.pd_root)}\t\t{self.pd_root}')
+    #     nw.walk(self.pd_root, sn_action_fn=None, sn_fn_kwargs=None, net_action_fn=self.builder.get_inverted_inputs,
+    #             net_fn_kwargs={'res_lst': res})
+    #     nw.walk(self.pu_root, sn_action_fn=None, sn_fn_kwargs=None, net_action_fn=self.builder.get_inverted_inputs,
+    #             net_fn_kwargs={'res_lst': res})
+    #     return res
+
+    @staticmethod
+    def is_single_instance(sn: SerialNodes):
+        return len(sn.nodes) == 1
+
+    @staticmethod
+    def get_next_net(sn: SerialNodes):
+        if sn.next_net is None:
+            return '0'
+        else:
+            return f'{sn.next_net.id}'
+
+    @staticmethod
+    def not_converting(name: str):
+        if name[0] == '!':
+            return 'n' + name[1:]
+        else:
+            return name
+
+    def spice_instances_filling(self, current_net: Net):
+        for i in range(len(current_net.node_lists)):
+            if self.is_single_instance(current_net.node_lists[i]):
+                inst_type = 'X'
+                name = f'M{self.name_idx}'
+                nxt_net = self.get_next_net(current_net.node_lists[i])
+                tran_name = self.not_converting(current_net.node_lists[i].nodes[0])
+                cons = [current_net.id, tran_name, nxt_net, '0']
+                self.name_idx += 1
+                self.pd_instances.append(Instance(inst_type, name, cons, self.nfet))
+            else:
+                print('dude')
+
+
 
     def parse(self):
         self.fd.write(self.lib)
