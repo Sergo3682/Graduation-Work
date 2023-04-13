51bea91b0a92af744112bdfa3a4c1614015bb3cc
Sergey Rogov <rogovsergey2001@gmail.com>
2023-04-13 21:38:23 +0300

    reading cfg files and creating ngspice sbckt

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from truthtable import TruthTable
 from bitvector import BitVector
 from schematicbuilder import SchematicBuilder
-from parser import Parser
+from generator import Generator
+import yaml
+import argparse
 
 if __name__ == '__main__':
 
-    bv = BitVector(0b00101101, 8)
-    tt = TruthTable(bv, ['A', 'B', 'C'])
+    out_file = 'netlist.sp'
+    spice_lib = '~/VSU224_Sky130_fd_pr/models/sky130.lib.spice'
+    cfg = {}
+    with open('spgen_cfg.yml') as fd_cfg:
+        cfg = yaml.load(fd_cfg, Loader=yaml.SafeLoader)
+    bv = BitVector(0b10010110, 8)
+    tt = TruthTable(bv, ['CI', 'A', 'B'])
+
     sb = SchematicBuilder(tt)
     sb.build_pull_down_network()
     sb.build_pull_up_network()
-    p = Parser('netlist.sp', sb, '~/VSU224_Sky130_fd_pr/models/sky130.lib.spice')
-    p.parse()
+
+    gen = Generator(out_file, sb, spice_lib, cfg)
+    gen.generate_subcircuit()
+    gen.test_single_subckt()
