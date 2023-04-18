c28670c4e104e25c208a67100f188e297317e2a7
Sergey Rogov <rogovsergey2001@gmail.com>
2023-04-19 00:12:12 +0300

    added function 2^n

--- helpers.py
+++ helpers.py
@@ -22,20 +22,8 @@ def print_partial_truth_table(list_of_tuples: list):
         i += 1
     print(res_str)
 
-
-#def width_of_circuit(net_list: list):
-#    if type(net_list) == list:
-#        for net in net_list:
-#            if type(net) != Net:
-#                raise TypeError('Your input must be \'list\' of \'Net()\'')
-#    else:
-#        raise TypeError('Your input must be \'list\' of \'Net()\'')
-#    i = 0
-#    width = 0
-#    for i in range(len(net_list)):
-#        width += len(net_list[i].node_lists)
-#    return width - i
-
-
 def insert(source_str, insert_str, pos):
     return source_str[:pos] + insert_str + source_str[pos:]
+
+def two_to_the_power_of(n):
+        return 2 << (n - 1)
