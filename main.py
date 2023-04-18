e79f13f04e6ebc75d08181b6a72aeb48e503ca04
Sergey Rogov <rogovsergey2001@gmail.com>
2023-04-19 00:12:51 +0300

    work in progress...

--- main.py
+++ main.py
@@ -14,30 +14,40 @@ def gen_input_name(bv_size):
         res.append(chr(j + i))
     return res
 
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='Digital library compiler')
-    parser.add_argument('-c', '--cfg', type=str, default='spgen_cfg.yml',
+def parser_init():
+    out_parser = argparse.ArgumentParser(description='Digital library compiler')
+    out_parser.add_argument('-c', '--cfg', type=str, default='spgen_cfg.yml',
                         help='set custom config file (default: spgen_cfg.yml)')
-    parser.add_argument('-o', '--out', type=str, default='netlist.sp',
+    out_parser.add_argument('-o', '--out', type=str, default='netlist.sp',
                         help='set custom output file (default: netlist.sp)')
-    parser.add_argument('--gen_cell', type=str, default=None,
+    out_parser.add_argument('--gen_cell', type=str, default=None,
                         help='generate single cell for custom bitvector')
-    parser.add_argument('--input_names', type=str, default=None,
+    out_parser.add_argument('--input_names', type=str, default=None,
                         help='set custom input names for a single cell')
-    parser.add_argument('-spice_lib_path', '--spice_lib_path', type=str,
+    out_parser.add_argument('-spice_lib_path', '--spice_lib_path', type=str,
                         default='~/VSU224_Sky130_fd_pr/models/sky130.lib.spice',
                         help='set custom path to spice library for circuit simulation')
-    #todo gen_lib 3
-    #todo parser_init function
-    #todo generator_init function
+    out_parser.add_argument('-lib', '--gen_lib', type=int, default=None,
+                        help='generate subcircuits for a whole library')
+    return out_parser
+
+# todo generator_init function
+def generator_init():
+    pass
+
+if __name__ == '__main__':
+    parser = parser_init()
+
     input_args = parser.parse_args()
     spice_lib = input_args.spice_lib_path
+
     with open(input_args.cfg) as fd_cfg:
         cfg = yaml.load(fd_cfg, Loader=yaml.SafeLoader)
 
-    if input_args.gen_cell is not None:
+    if (input_args.gen_cell is not None) and (input_args.gen_lib is not None):
+        raise KeyError(f"You can't generate single circuit and whole library at the same time")
+
+    elif input_args.gen_cell is not None:
         val = int(input_args.gen_cell, 2)
         num_of_inputs = log_2(len(input_args.gen_cell))
         bv = BitVector(val, len(input_args.gen_cell))
@@ -46,14 +56,16 @@ if __name__ == '__main__':
         else:
             input_names = gen_input_name(bv.size)
         tt = TruthTable(bv, input_names)
+        sb = SchematicBuilder(tt)
+        sb.build_pull_down_network()
+        sb.build_pull_up_network()
 
-    if (input_args.gen_cell is not None) and (input_args.gen_lib is not None):
-        raise KeyError(f"You can't generate single circuit and whole library at the same time")
+        gen = Generator(input_args.out, sb, spice_lib, cfg)
+        gen.generate_subcircuit(input_args.gen_cell)
+        fd_test = open(f'test.sp', 'w')
+        gen.test_single_subckt(fd_test, input_args.gen_cell)
+        fd_test.close()
 
-    sb = SchematicBuilder(tt)
-    sb.build_pull_down_network()
-    sb.build_pull_up_network()
-
-    gen = Generator(input_args.out, sb, spice_lib, cfg)
-    gen.generate_subcircuit()
-    gen.test_single_subckt()
+    elif input_args.gen_lib is not None:
+        max_input = input_args.gen_lib
+        # todo this
